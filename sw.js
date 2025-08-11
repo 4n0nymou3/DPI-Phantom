644c48310ac07e59f098f71a56752000992dd3d1
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-08-12 02:53:51 +0330

    sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,17 @@
-const CACHE_NAME = 'phantom-chainer-cache-v4';
-const URLS_TO_CACHE = [
+const CACHE_NAME = 'phantom-chainer-cache-v5';
+const APP_SHELL_URLS = [
     '/',
     '/index.html',
     '/css/style.css',
-    '/js/script.js',
-    'https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-    'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&family=Roboto:wght@400;700&display=swap'
+    '/js/script.js'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
         .then(cache => {
-            console.log('Opened cache and caching app shell');
-            return cache.addAll(URLS_TO_CACHE);
+            console.log('Installing Service Worker and caching App Shell...');
+            return cache.addAll(APP_SHELL_URLS);
         })
     );
 });
@@ -38,18 +35,22 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
         .then(cachedResponse => {
-            const fetchPromise = fetch(event.request).then(networkResponse => {
-                const responseToCache = networkResponse.clone();
-                caches.open(CACHE_NAME).then(cache => {
-                    cache.put(event.request, responseToCache);
-                });
-                return networkResponse;
-            }).catch(err => {
-                console.log('Fetch failed; returning offline page instead.', err);
+            if (cachedResponse) {
                 return cachedResponse;
-            });
-
-            return cachedResponse || fetchPromise;
+            }
+            return fetch(event.request).then(
+                networkResponse => {
+                    if (!networkResponse || networkResponse.status !== 200) {
+                        return networkResponse;
+                    }
+                    const responseToCache = networkResponse.clone();
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(event.request, responseToCache);
+                        });
+                    return networkResponse;
+                }
+            );
         })
     );
 });
